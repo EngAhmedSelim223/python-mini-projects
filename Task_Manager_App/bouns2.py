8b4f6d40d9725820370a1f4d9aab7f53c2424543
Ahmed Selim <ahmedselimelsayed@gmail.com>
2026-07-15 15:09:55 +0300

    Section2

---
+++ Task_Manager_App/bouns2.py
@@ -0,0 +1,7 @@
+password = input("Enter a password: ").lower()
+
+
+while password !=  "ahmed":
+    password = input("Enter a password: ").lower()
+else:
+    print("Welcome Mr: Ahmed")
--- Task_Manager_App/main.py
+++ Task_Manager_App/main.py
@@ -1,8 +1,8 @@
 user_prompt = "Enter a todo: "
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
-todos = [todo1, todo2, todo3]
-print(todos)
-print(type(user_prompt))
-print(type(todos))
+todos = []
+
+while True:
+    todo = input(user_prompt)
+    print(todo.title())
+    todos.append(todo)
+    print(todos)
