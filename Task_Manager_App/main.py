2044e8e721d5340f284492b85cc3d301524e48e9
Ahmed Selim <ahmedselimelsayed@gmail.com>
2026-07-16 11:21:17 +0300

    Section3

--- Task_Manager_App/main.py
+++ Task_Manager_App/main.py
@@ -1,8 +1,20 @@
-user_prompt = "Enter a todo: "
 todos = []
 
 while True:
-    todo = input(user_prompt)
-    print(todo.title())
-    todos.append(todo)
-    print(todos)
+    user_action = input("Type add, show or exit: ")
+    
+    match user_action:
+        case "add":
+            todo = input("Enter a todo: ").strip().lower()
+            todos.append(todo)
+            
+        
+        case "show":
+            for index, item in enumerate(todos):
+                print(f"{index + 1} - {item}")
+        case "exit":
+            break
+        case _:
+            print("Please Enter a valid option add, show or exit")
+
+print("Thanks for using our task manager app :)")
